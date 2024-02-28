f402146861a2f927a8f67ed958f0b37dc2ba698e
Joseph Muller <jtmuller5@gmail.com>
2024-02-27 21:39:01 -0500

    Initial commit

---
+++ SwiftUICheatSheet/SwiftUICheatSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICheatSheetApp.swift
+//  SwiftUICheatSheet
+//
+//  Created by Joseph Muller on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICheatSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
